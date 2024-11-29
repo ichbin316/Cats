5d054d233b67cf6afc65471ba6e66a672c8fdefc
Kaleb <Kaleb@JUNIOR>
2024-11-29 16:57:26 -0600

    Add project files.

---
+++ Cats/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Cats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+
+            
+            Cats miso = new Cats("miso");
+
+            Console.ReadKey();
+
+            miso.speak();
+
+            bool sleeping = true;
+
+
+            if (sleeping == true) { 
+                
+                miso.nap();
+            }  
+            
+
+            Console.ReadKey();
+            
+
+        }
+    }
+    class Cats
+
+    {
+        // Everything that exist needs a name 
+        public string name;
+        public Cats(string myname)
+        {
+            name = myname;
+
+        }
+
+        // A cat needs a voice!
+        public void speak()
+        {
+
+            Console.WriteLine("hello my name is " + name);
+
+
+
+        }
+
+        public void nap()
+        {
+
+            Console.WriteLine(name + " is sleeping");
+        }
+
+
+
+
+    }
+}
+
